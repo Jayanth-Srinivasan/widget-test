b06dc46ae1db3816cf029875b63d31818b228e03
Jayanth Srinivasan <81894141+Jayanth-Srinivasan@users.noreply.github.com>
2025-03-14 13:07:31 +0000

    fix: added function

--- provider.js
+++ provider.js
@@ -1,10 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-    alert("script loaded");
-    
-    var targetDiv = document.querySelector(".trigger-div");
-    if (targetDiv) {
-        targetDiv.textContent = "Hello, this text was injected from an external CDN!";
-    } else {
-        console.error("err");
+(function () {
+    alert("Script loaded");
+
+    function updateDiv() {
+        var targetDiv = document.querySelector(".trigger-div");
+        if (targetDiv) {
+            targetDiv.textContent = "Hello, this text was injected from an external CDN!";
+        } else {
+            console.error("Target div with class 'trigger-div' not found.");
+        }
     }
-});
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", updateDiv);
+    } else {
+        updateDiv();
+    }
+})();
