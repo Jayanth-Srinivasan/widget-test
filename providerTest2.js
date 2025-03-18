a26bd46e0d18d91360bc8d84f8be644dfda208f7
Jayanth Srinivasan <81894141+Jayanth-Srinivasan@users.noreply.github.com>
2025-03-18 03:15:52 +0000

    applied styles

--- providerTest2.js
+++ providerTest2.js
@@ -8,13 +8,15 @@
             return;
         }
 
-        targetDiv.textContent = "Loading data from API...";
-
         try {
             const response = await fetch("https://official-joke-api.appspot.com/random_joke");
             const data = await response.json();
 
-            const injectedText = `Hello from Jay, Here's a joke: ${data.setup} — ${data.punchline}`;
+            const injectedText = `Hello from CDN! Here's a joke:\n${data.setup} — ${data.punchline}`;
+            
+            targetDiv.style.color = "red";
+            targetDiv.style.fontFamily = "monospace";
+            targetDiv.style.whiteSpace = "pre-line"; 
             targetDiv.textContent = injectedText;
         } catch (error) {
             console.error("Failed to fetch from API:", error);
